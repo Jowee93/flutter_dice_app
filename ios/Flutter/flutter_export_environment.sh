39a17cd96c78b5d571210dc642f985189682490a
YEAP JO WEE <jowee@YEAPs-MacBook-Pro.local>
2019-12-15 11:31:39 +0800

    Add randomizer to two dice

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jowee/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/jowee/Documents/my-projects/flutter/dice_app/dicee-flutter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/jowee/Documents/my-projects/flutter/dice_app/dicee-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/jowee/development/flutter/bin/cache/artifacts/engine/ios"
